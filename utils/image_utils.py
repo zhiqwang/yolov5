daa930009f7d4f65316fc917ffd53ad59b00c015
zhiqwang <zhiqwang@outlook.com>
2020-12-26 04:20:33 -0500

    Clean and refactor codes

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -10,7 +10,7 @@ from utils.plots import color_list
 def cv2_imshow(a):
     """A replacement for cv2.imshow() for use in Jupyter notebooks.
     Args:
-        a : np.ndarray. shape (N, M) or (N, M, 1) is an NxM grayscale image. shape
+        a: np.ndarray. shape (N, M) or (N, M, 1) is an NxM grayscale image. shape
         (N, M, 3) is an NxM BGR color image. shape (N, M, 4) is an NxM BGRA color
         image.
     """
@@ -48,20 +48,6 @@ def decode_target(targets, sizes):
     return targets
 
 
-def bbox_visualize(imgs, targets):
-    targets = to_numpy(targets)
-    h, w = imgs.shape[-2:]
-    imgs = decode_image(imgs)
-    targets = decode_target(targets, (h, w))
-
-    for i in range(imgs.shape[0]):
-        img = imgs[i].astype(np.uint8)
-        img = img[..., ::-1]
-        target = targets[targets[:, 0] == i][:, 2:]
-        target = box_cxcywh_to_xyxy(target)
-        show_bbox(img, target)
-
-
 def to_numpy(tensor):
     if tensor.requires_grad:
         return tensor.detach().cpu().numpy()
@@ -69,125 +55,122 @@ def to_numpy(tensor):
         return tensor.cpu().numpy()
 
 
+def restore_label(target, feature_map_sizes, image_sizes):
+    h, w = image_sizes
+    target = target / np.array(feature_map_sizes)[[3, 2, 3, 2]]
+    target = target * np.array([w, h, w, h], np.float32)
+    target = box_cxcywh_to_xyxy(target)
+    return target
+
+
+def restore_anchor(anchor, grid_x, grid_y, stride, feature_map_size, image_sizes):
+    anchor *= stride
+    h, w = image_sizes
+    anchor_restored = np.stack([grid_y, grid_x], axis=1)
+
+    anchor_restored = anchor_restored / np.array(feature_map_size, np.float)[[3, 2]]
+    anchor_restored = anchor_restored * np.array([w, h], np.float32)
+    anchor_restored = np.concatenate([anchor_restored, anchor], axis=1)
+    anchor_restored = box_cxcywh_to_xyxy(anchor_restored)
+
+    return anchor_restored
+
+
 def anchor_match_visualize(images, targets, indices, anchors, pred):
-    h, w = images.shape[-2:]
+    image_sizes = images.shape[-2:]
     images = decode_image(images)
 
     strdie = [8, 16, 32]
 
     images_with_anchor = []
-    # 对每张图片进行可视化
+    # for loop all images
     for j in range(images.shape[0]):
-        img = images[j].astype(np.uint8)[..., ::-1]
+        image = images[j].astype(np.uint8)[..., ::-1]
 
-        # 对每个预测尺度进行单独可视化
-        vis_imgs = []
-        for i in range(3):  # i=0 检测小物体，i=1 检测中等尺度物体，i=2 检测大物体
-            s = strdie[i]
-            # anchor尺度
-            gain = np.array(pred[i].shape)[[3, 2, 3, 2]]
+        # Visualize each prediction scale individually
+        vis_all_scale_images = []
+        # i = 0 is associate with small objects,
+        # i = 1 is associate with intermediate objects,
+        # i = 2 is associate with large objects
+        for i in range(3):
+            image_per_scale = image.copy()
+            stride = strdie[i]
+            # anchor scale
             b, _, grid_x, grid_y = indices[i]
 
-            b = to_numpy(b)
-            grid_x = to_numpy(grid_x)
-            grid_y = to_numpy(grid_y)
-            anchor = to_numpy(anchors[i])
-            target = to_numpy(targets[i])
+            b, grid_x, grid_y, anchor, target = map(to_numpy, [b, grid_x, grid_y, anchors[i], targets[i]])
 
-            # 找出对应图片对应分支的信息
+            # Find out the corresponding branch of one image
             idx = b == j
-            grid_x = grid_x[idx]
-            grid_y = grid_y[idx]
-            anchor = anchor[idx]
-            target = target[idx]
+            grid_x, grid_y, anchor, target = grid_x[idx], grid_y[idx], anchor[idx], target[idx]
 
-            # 还原到原图尺度进行可视化
-            target /= gain
-            target *= np.array([w, h, w, h], np.float32)
-            target = box_cxcywh_to_xyxy(target)
+            # Restore to the original image scale for visualization
+            target = restore_label(target, pred[i].shape, image_sizes)
 
-            # label 可视化
-            img = show_bbox(img.copy(), target, color=(0, 0, 255), is_show=False)
+            # visualize labels
+            image_per_scale = overlay_bbox(
+                image_per_scale,
+                target,
+                color=(0, 0, 255),
+                thickness=3,
+                with_mask=False,
+            )
 
-            # anchor 需要考虑偏移，在任何一层，每个bbox最多3*3=9个anchor进行匹配
-            anchor *= s
-            anchor_bbox = np.stack([grid_y, grid_x], axis=1)
-            k = np.array(pred[i].shape, np.float)[[3, 2]]
-            anchor_bbox = anchor_bbox / k
-            anchor_bbox *= np.array([w, h], np.float32)
-            anchor_bbox = np.concatenate([anchor_bbox, anchor], axis=1)
-            anchor_bbox = box_cxcywh_to_xyxy(anchor_bbox)
+            # The anchors need to restore the offset.
+            # In eacy layer there has at most 3x3=9 anchors for matching.
+            anchor_restored = restore_anchor(anchor, grid_x, grid_y, stride, pred[i].shape, image_sizes)
 
-            # 正样本anchor可视化
-            img = show_bbox(img, anchor_bbox, color=(0, 255, 255), is_show=False)
-            vis_imgs.append(img)
+            # visualize positive anchor
+            image_per_scale = overlay_bbox(image_per_scale, anchor_restored)
+            vis_all_scale_images.append(image_per_scale)
 
-        per_image_with_anchor = merge_images_with_boundary(vis_imgs)
+        per_image_with_anchor = merge_images_with_boundary(vis_all_scale_images)
         images_with_anchor.append(per_image_with_anchor)
 
     return images_with_anchor
 
 
-def show_bbox(image, bboxs_list, color=None,
-              thickness=1, font_scale=0.3, wait_time_ms=0, names=None,
-              is_show=True, is_without_mask=False):
+def overlay_bbox(image, bboxs_list, color=None, thickness=2, font_scale=0.3, with_mask=False):
     """
     Visualize bbox in object detection by drawing rectangle.
 
-    :param image: numpy.ndarray.
-    :param bboxs_list: list: [pts_xyxy, prob, id]: label or prediction.
-    :param color: tuple.
-    :param thickness: int.
-    :param fontScale: float.
-    :param wait_time_ms: int
-    :param names: string: window name
-    :param is_show: bool: whether to display during middle process
-    :return: numpy.ndarray
+    Args:
+        image: numpy.ndarray.
+        bboxs_list: list: [pts_xyxy, prob, id]: label or prediction.
+        color: tuple.
+        thickness: int.
+        font_scale: float.
+        with_mask: draw as mask.
+
+    Return:
+        numpy.ndarray
     """
     assert image is not None
     font = cv2.FONT_HERSHEY_SIMPLEX
-    image_copy = image.copy()
     txt = ''
+    colors = color_list()  # list of colors
+
     for bbox in bboxs_list:
         if len(bbox) == 5:
             txt = '{:.3f}'.format(bbox[4])
         elif len(bbox) == 6:
             txt = 'p={:.3f}, id={:.3f}'.format(bbox[4], bbox[5])
         bbox_f = np.array(bbox[:4], np.int32)
-        if color is None:
-            colors = color_list()  # list of colors
-            color = colors[np.random.randint(0, len(colors))]
 
-        if not is_without_mask:
-            image_copy = cv2.rectangle(
-                image_copy,
-                (bbox_f[0], bbox_f[1]),
-                (bbox_f[2], bbox_f[3]),
-                color,
-                thickness,
-            )
-        else:
-            mask = np.zeros_like(image_copy, np.uint8)
-            mask1 = cv2.rectangle(
-                mask,
-                (bbox_f[0], bbox_f[1]),
-                (bbox_f[2], bbox_f[3]),
-                color,
-                -1,
-            )
-            mask = np.zeros_like(image_copy, np.uint8)
-            mask2 = cv2.rectangle(
-                mask,
-                (bbox_f[0], bbox_f[1]),
-                (bbox_f[2], bbox_f[3]),
-                color,
-                thickness,
-            )
-            mask2 = cv2.addWeighted(mask1, 0.5, mask2, 8, 0.0)
-            image_copy = cv2.addWeighted(image_copy, 1.0, mask2, 0.6, 0.0)
+        mask = np.zeros_like(image, np.uint8)
+        mask = cv2.rectangle(
+            mask if with_mask else image,
+            (bbox_f[0], bbox_f[1]),
+            (bbox_f[2], bbox_f[3]),
+            color=(color or colors[np.random.randint(0, len(colors))]),
+            thickness=(-1 if with_mask else thickness),
+        )
+        if with_mask:
+            image = cv2.addWeighted(image, 1.0, mask, 0.6, 0)
+
         if len(bbox) == 5 or len(bbox) == 6:
             cv2.putText(
-                image_copy,
+                image,
                 txt,
                 (bbox_f[0], bbox_f[1] - 2),
                 font,
@@ -196,69 +179,64 @@ def show_bbox(image, bboxs_list, color=None,
                 thickness=thickness,
                 lineType=cv2.LINE_AA,
             )
-    if is_show:
-        merge_images_with_boundary(image_copy)
-    return image_copy
+
+    return image
 
 
-def merge_images_with_boundary(imgs, row_col_num=(1, -1)):
+def merge_images_with_boundary(images_list, row_col_num=(1, -1)):
     """
-        Displays an image or a list of images in specified windows or self-initiated windows.
-        You can also control display wait time by parameter 'wait_time_ms'.
-        Additionally, this function provides an optional parameter 'is_merge' to
-        decide whether to display all imgs in a particular window 'merge'.
-        Besides, parameter 'row_col_num' supports user specified merge format.
-        Notice, specified format must be greater than or equal to imgs number.
+    To merge a list of images.
 
-        :param imgs: numpy.ndarray or list.
-        :param window_names: specified or None, if None, function will create different windows as '1', '2'.
-        :param wait_time_ms: display wait time.
-        :param is_merge: whether to merge all images.
-        :param row_col_num: merge format. default is (1, -1), image will line up to show.
-                            example=(2, 5), images will display in two rows and five columns.
-        """
-    if not isinstance(imgs, list):
-        imgs = [imgs]
+    Additionally, parameter ``row_col_num`` supports user specified merge format.
+    Notice, specified format must be greater than or equal to images number.
 
-    img_merged = merge_images(imgs, row_col_num)
-    return img_merged
-
-
-def merge_images(imgs, row_col_num):
+    Args:
+        image_lists: list of np.ndarray.
+        row_col_num: merge format. default is (1, -1), image will line up to show.
+            when set example=(2, 5), images will display in two rows and five columns.
     """
-        Merges all input images as an image with specified merge format.
+    if not isinstance(images_list, list):
+        images_list = [images_list]
 
-        :param imgs : img list
-        :param row_col_num : number of rows and columns displayed
-        :return img : merges img
-        """
+    images_merged = merge_images(images_list, row_col_num)
+    return images_merged
 
-    length = len(imgs)
+
+def merge_images(images_list, row_col_num):
+    """
+    Merges all input images as an image with specified merge format.
+
+    Args:
+        images_list: images list
+        row_col_num: number of rows and columns displayed
+    Return:
+        image: merges image
+    """
+
+    num_images = len(images_list)
     row, col = row_col_num
 
     assert row > 0 or col > 0, 'row and col cannot be negative at same time!'
-    colors = color_list()  # list of colors
-    color = colors[np.random.randint(0, len(colors))]
 
-    for img in imgs:
-        cv2.rectangle(img, (0, 0), (img.shape[1], img.shape[0]), color)
+    for image in images_list:
+        cv2.rectangle(image, (0, 0), (image.shape[1], image.shape[0]), (255, 0, 255))
 
-    if row_col_num[1] < 0 or length < row:
-        img_merged = np.hstack(imgs)
-    elif row_col_num[0] < 0 or length < col:
-        img_merged = np.vstack(imgs)
+    if row_col_num[1] < 0 or num_images < row:
+        images_merged = np.hstack(images_list)
+    elif row_col_num[0] < 0 or num_images < col:
+        images_merged = np.vstack(images_list)
     else:
-        assert row * col >= length, 'Imgs overboundary, not enough windows to display all imgs!'
+        assert row * col >= num_images, 'Images overboundary, not enough windows to display all images!'
 
-        fill_img_list = [np.zeros(imgs[0].shape, dtype=np.uint8)] * (row * col - length)
-        imgs.extend(fill_img_list)
+        fill_img_list = [np.zeros(images_list[0].shape, dtype=np.uint8)] * (row * col - num_images)
+        images_list.extend(fill_img_list)
         merge_imgs_col = []
         for i in range(row):
             start = col * i
             end = col * (i + 1)
-            merge_col = np.hstack(imgs[start: end])
+            merge_col = np.hstack(images_list[start: end])
             merge_imgs_col.append(merge_col)
 
-        img_merged = np.vstack(merge_imgs_col)
+        images_merged = np.vstack(merge_imgs_col)
 
-    return img_merged
+    return images_merged
